9d7f8bc10a7cdab552b9cae417e90261bcb84620
Dylan Leard <dylan@button.is>
2019-07-31 15:11:32 -0700

    wrote a sample test, testing the result of the sql->mbql translater on a simple sql query

--- test/test.test.js
+++ test/test.test.js
@@ -1,5 +1,26 @@
-// import { exportAllDeclaration } from "@babel/types";
+const sql_to_mbql = require('../transform/sql_to_mbql_adv');
 
-test('test the jest', () => {
-  expect(2+2).toBe(4);
+const question = {
+  sql: 'select fuel.abc, fuel.cde, report.fgh from ggircs.fuel left join ggircs.report on fuel.report_id = report.id',
+  dataset_query:{
+    query: {
+      fields:{
+        0:[123]
+      }
+    }
+  }
+}
+
+test('test the sql_to_mbql translator', () => {
+  expect(sql_to_mbql(question))
+  .toBe(
+    {"aggregation": [],
+     "breakout": [],
+     "columns": [["abc"], ["cde"], ["report_id"]],
+     "fields": ["abc", "cde", ["fk->", "report_id", "report.fgh"]],
+     "filter": [],
+     "foreign_columns": [["report", "fgh"]],
+     "order-by": [],
+     "source_table": ["fuel"]}
+  );
 });
