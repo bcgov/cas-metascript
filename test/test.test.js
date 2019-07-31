68cc0cdf1a959574f58755c35a88446698403968
Dylan Leard <dylan@button.is>
2019-07-31 15:16:17 -0700

    edited sample test

--- test/test.test.js
+++ test/test.test.js
@@ -1,19 +1,19 @@
 const sql_to_mbql = require('../transform/sql_to_mbql_adv');
 
-const question = {
-  sql: 'select fuel.abc, fuel.cde, report.fgh from ggircs.fuel left join ggircs.report on fuel.report_id = report.id',
-  dataset_query:{
-    query: {
-      fields:{
-        0:[123]
+// Test the sql -> mbql translator
+test('test the sql_to_mbql translator', () => {
+  const question = {
+    sql: 'select fuel.abc, fuel.cde, report.fgh from ggircs.fuel left join ggircs.report on fuel.report_id = report.id',
+    dataset_query:{
+      query: {
+        fields:{
+          0:[123]
+        }
       }
     }
   }
-}
-
-test('test the sql_to_mbql translator', () => {
   expect(sql_to_mbql(question))
-  .toBe(
+  .toStrictEqual(
     {"aggregation": [],
      "breakout": [],
      "columns": [["abc"], ["cde"], ["report_id"]],
