60823712b9f946db5cd53179d0556af760b88616
Dylan Leard <dylan@button.is>
2019-08-02 11:21:23 -0700

    added tests for getSession

--- test/api_calls_test/get_session.test.js
+++ test/api_calls_test/get_session.test.js
@@ -1,1 +1,27 @@
-const morph = require('mock-env').morph;
+const mockedEnv = require('mocked-env');
+const getSession = require('../../api_calls/get_session');
+let restore;
+
+describe('getSession() tests', () => {
+  test('getSession fails with bad credentials', async () => {
+    restore = mockedEnv({
+      METABASE_USERNAME: 'bad@credentials.is',
+      METABASE_PASSWORD: 'accessdenied',
+      URL:'https://metabase-wksv3k-test.pathfinder.gov.bc.ca/api'
+    });
+    const session = await getSession();
+    expect(session.id).toBeUndefined();
+    expect(session.errors).toBeDefined();
+    restore();
+  });
+
+  test('getSession returns a session id with good credentials', async () => {
+    restore = mockedEnv ({
+      URL:'https://metabase-wksv3k-test.pathfinder.gov.bc.ca/api'
+    });
+    const session = await getSession();
+    expect(session.id).toBeDefined();
+    expect(session.errors).toBeUndefined();
+    restore();
+  });
+});
