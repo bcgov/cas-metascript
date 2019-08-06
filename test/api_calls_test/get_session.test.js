a1e40d7434759c910fd71b74d053663f0d804516
Dylan Leard <dylan@button.is>
2019-08-06 16:50:27 -0700

    added session tests

--- test/api_calls_test/get_session.test.js
+++ test/api_calls_test/get_session.test.js
@@ -1,6 +1,7 @@
 const mockedEnv = require('mocked-env');
 const { setupRecorder } = require('nock-record');
 const getSession = require('../../api_calls/get_session');
+const fs = require('fs');
 
 const record = setupRecorder({ mode: 'record' });
 
@@ -42,11 +43,33 @@ describe('getSession() tests', () => {
     restore();
   });
 
-  xtest('getSession returns a cached session if session is still valid', () => {
+  test('getSession returns a cached session if session is still valid', async () => {
+    const restore = mockedEnv ({
+      METABASE_USERNAME: 'good@user.is',
+      METABASE_PASSWORD: 'goodpassword',
+      URL: process.env.TEST_URL
+    });
 
+    const cachedSession = JSON.parse(fs.readFileSync(`${__dirname}/../../api_calls/session/session.json`));
+    const session = await getSession();
+    expect(cachedSession['good@user.is']).toStrictEqual(session);
+    restore();
   });
 
-  xtest('getSession overwrites the session if forced', () => {
+  test('getSession overwrites the session if forced', async () => {
+    const restore = mockedEnv ({
+      METABASE_USERNAME: 'good@user.is',
+      METABASE_PASSWORD: 'goodpassword',
+      URL: process.env.TEST_URL
+    });
 
+    const cachedSession = JSON.parse(fs.readFileSync(`${__dirname}/../../api_calls/session/session.json`));
+    const  { completeRecording, assertScopesFinished } = await record('force session');
+    const session = await getSession({force: true});
+    completeRecording();
+
+    expect(cachedSession['good@user.is'].id).not.toEqual(session.id);
+    assertScopesFinished();
+    restore();
   });
 });
