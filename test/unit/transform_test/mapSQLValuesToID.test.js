218363cc8d0d9b274cc47dbfa2060d24da7cfe92
Dylan Leard <dylan@button.is>
2019-08-07 16:52:25 -0700

    added fk test to mapSQLValuesToID tests

--- test/unit/transform_test/mapSQLValuesToID.test.js
+++ test/unit/transform_test/mapSQLValuesToID.test.js
@@ -21,7 +21,7 @@ describe('mapSQLValuesToID tests', () => {
     callAPI.mockReset();
   });
 
-  test('fields in simple queries (no fk relations) can be converted to mbql field IDs' , async () => {
+  test('fields in simple queries (no fk relations) can be converted to corresponding mbql field IDs' , async () => {
     question.mbql.source_table = ['table1'];
     question.mbql.fields = ['abc', 'cde'];
     question.mbql.columns = [['abc'], ['cde']];
@@ -53,6 +53,52 @@ describe('mapSQLValuesToID tests', () => {
         "order-by": [],
         columns: [['abc',123], ['cde',456]],
         foreign_columns: []}}
+    );
+  });
+
+  test('fields in fk relations can be converted to corresponding mbql field IDs' , async () => {
+    question.mbql.source_table = ['table1'];
+    question.mbql.fields = [['fk->', 'abc', 'table2.cba'], ['fk->', 'cde', 'table2.edc']];
+    question.mbql.columns = [['abc'], ['cde']];
+    question.mbql.foreign_columns = [['table2', 'cba'], ['table2', 'edc']];
+    const returnValue = {
+      tables:[
+        {
+          name: 'table1',
+          id: 1,
+          schema: 'test',
+          fields:
+            [
+              { name: 'abc', id: 123 },
+              { name: 'cde', id: 456 }
+            ]
+        },
+        {
+          name: 'table2',
+          id: 2,
+          schema: 'test',
+          fields:
+            [
+              { name: 'cba', id: 321 },
+              { name: 'edc', id: 654 }
+            ]
+        }
+      ]
+    }
+    callAPI.mockImplementation(() => returnValue);
+    const session = {id: 12345};
+    expect(await mapSQLValuesToID(question, session))
+    .toStrictEqual(
+      {schema: 'test',
+      mbql: {
+        source_table:['table1', 1],
+        fields:[['fk->', ['field-id', 123], ['field-id', 321]], ['fk->', ['field-id', 456], ['field-id', 654]]],
+        filter: [],
+        breakout: [],
+        aggregation: [],
+        "order-by": [],
+        columns: [['abc', 123], ['cde', 456]],
+        foreign_columns: [['table2', 'cba', 321], ['table2', 'edc', 654]]}}
     );
   });
 });
