f6e8b73c5dc90cfbe6cc62ba29bec63a7d6199f9
Dylan Leard <dylan@button.is>
2019-08-08 14:36:27 -0700

    added unit tests for mapSQLValuesToID

--- test/unit/transform_test/mapSQLValuesToID.test.js
+++ test/unit/transform_test/mapSQLValuesToID.test.js
@@ -4,101 +4,281 @@ const util = require('util');
 jest.mock('../../../api_calls/call_api');
 
 describe('mapSQLValuesToID tests', () => {
-  const question = {
-    schema: 'test',
-    mbql:{
-      source_table: [],
-      fields: [],
-      filter: [],
-      breakout: [],
-      aggregation: [],
-      "order-by": [],
-      columns: [],
-      foreign_columns: []
-    }  
+  // mock value to be returned when mapSQLValuesToID calls callAPI in order to get the table/field metadata
+  const returnValue = {
+    tables:[
+      {
+        name: 'table1',
+        id: 1,
+        schema: 'test',
+        fields:
+          [
+            { name: 'abc', id: 123 },
+            { name: 'cde', id: 456 }
+          ]
+      },
+      {
+        name: 'table2',
+        id: 2,
+        schema: 'test',
+        fields:
+          [
+            { name: 'cba', id: 321 },
+            { name: 'edc', id: 654 }
+          ]
+      }
+    ]
   }
+  let question;
+  
   beforeEach(() => {
+
     callAPI.mockReset();
-  });
-
-  test('fields in simple queries (no fk relations) can be converted to corresponding mbql field IDs' , async () => {
-    question.mbql.source_table = ['table1'];
-    question.mbql.fields = ['abc', 'cde'];
-    question.mbql.columns = [['abc'], ['cde']];
-    const returnValue = {
-      tables:[
-        {
-          name: 'table1',
-          id: 1,
-          schema: 'test',
-          fields:
-            [
-              { name: 'abc', id: 123 },
-              { name: 'cde', id: 456 }
-            ]
-        }
-      ]
-    }
-    callAPI.mockImplementation(() => returnValue);
-    const session = {id: 12345};
-    expect(await mapSQLValuesToID(question, session))
-    .toStrictEqual(
-      {schema: 'test',
-      mbql: {
-        source_table:['table1', 1],
-        fields:[['field-id', 123], ['field-id', 456]],
+    question = {
+      schema: 'test',
+      mbql:{
+        source_table: [],
+        fields: [],
         filter: [],
         breakout: [],
         aggregation: [],
         "order-by": [],
-        columns: [['abc',123], ['cde',456]],
-        foreign_columns: []}}
-    );
+        columns: [],
+        foreign_columns: []
+      }  
+    }
   });
 
-  test('fields in fk relations can be converted to corresponding mbql field IDs' , async () => {
-    question.mbql.source_table = ['table1'];
-    question.mbql.fields = [['fk->', 'abc', 'table2.cba'], ['fk->', 'cde', 'table2.edc']];
-    question.mbql.columns = [['abc'], ['cde']];
-    question.mbql.foreign_columns = [['table2', 'cba'], ['table2', 'edc']];
-    const returnValue = {
-      tables:[
-        {
-          name: 'table1',
-          id: 1,
-          schema: 'test',
-          fields:
-            [
-              { name: 'abc', id: 123 },
-              { name: 'cde', id: 456 }
-            ]
-        },
-        {
-          name: 'table2',
-          id: 2,
-          schema: 'test',
-          fields:
-            [
-              { name: 'cba', id: 321 },
-              { name: 'edc', id: 654 }
-            ]
-        }
-      ]
-    }
-    callAPI.mockImplementation(() => returnValue);
-    const session = {id: 12345};
-    expect(await mapSQLValuesToID(question, session))
-    .toStrictEqual(
-      {schema: 'test',
-      mbql: {
-        source_table:['table1', 1],
-        fields:[['fk->', ['field-id', 123], ['field-id', 321]], ['fk->', ['field-id', 456], ['field-id', 654]]],
-        filter: [],
-        breakout: [],
-        aggregation: [],
-        "order-by": [],
-        columns: [['abc', 123], ['cde', 456]],
-        foreign_columns: [['table2', 'cba', 321], ['table2', 'edc', 654]]}}
-    );
+  describe('fields (select statement) tests', () => {
+    test('named fields columns in simple queries (no fk relations) can be converted to corresponding mbql field IDs' , async () => {
+      question.mbql.source_table = ['table1'];
+      question.mbql.fields = ['abc', 'cde'];
+      question.mbql.columns = [['abc'], ['cde']];
+
+      callAPI.mockImplementation(() => returnValue);
+      const session = {id: 12345};
+      expect(await mapSQLValuesToID(question, session))
+      .toStrictEqual(
+        {schema: 'test',
+        mbql: {
+          source_table:['table1', 1],
+          fields:[['field-id', 123], ['field-id', 456]],
+          filter: [],
+          breakout: [],
+          aggregation: [],
+          "order-by": [],
+          columns: [['abc',123], ['cde',456]],
+          foreign_columns: []}}
+      );
+    });
+
+    test('named fields columns in fk relations can be converted to corresponding mbql field IDs' , async () => {
+      question.mbql.source_table = ['table1'];
+      question.mbql.fields = [['fk->', 'abc', 'table2.cba'], ['fk->', 'cde', 'table2.edc']];
+      question.mbql.columns = [['abc'], ['cde']];
+      question.mbql.foreign_columns = [['table2', 'cba'], ['table2', 'edc']];
+
+      callAPI.mockImplementation(() => returnValue);
+      const session = {id: 12345};
+      expect(await mapSQLValuesToID(question, session))
+      .toStrictEqual(
+        {schema: 'test',
+        mbql: {
+          source_table:['table1', 1],
+          fields:[['fk->', ['field-id', 123], ['field-id', 321]], ['fk->', ['field-id', 456], ['field-id', 654]]],
+          filter: [],
+          breakout: [],
+          aggregation: [],
+          "order-by": [],
+          columns: [['abc', 123], ['cde', 456]],
+          foreign_columns: [['table2', 'cba', 321], ['table2', 'edc', 654]]}}
+      );
+    });
+  });
+
+  describe('filter (where clause) tests', () => {
+    test('named filter columns can be converted to corresponding mbql field IDs' , async () => {
+      question.mbql.source_table = ['table1'];
+      question.mbql.filter = [['=', 'abc', 1]]
+      question.mbql.columns = [['abc']];
+
+      callAPI.mockImplementation(() => returnValue);
+      const session = {id: 12345};
+      expect(await mapSQLValuesToID(question, session))
+      .toStrictEqual(
+        {schema: 'test',
+        mbql: {
+          source_table:['table1', 1],
+          fields:[],
+          filter: [['=', ['field-id', 123], 1]],
+          breakout: [],
+          aggregation: [],
+          "order-by": [],
+          columns: [['abc',123]],
+          foreign_columns: []}}
+      );
+    });
+
+    test('named filter columns with foreign keys can be converted to corresponding mbql field IDs' , async () => {
+      question.mbql.source_table = ['table1'];
+      question.mbql.filter = [['=', ['fk->', 'abc', 'table2.cba'], 1]]
+      question.mbql.columns = [['abc']];
+      question.mbql.foreign_columns = [['table2', 'cba']];
+
+      callAPI.mockImplementation(() => returnValue);
+      const session = {id: 12345};
+      expect(await mapSQLValuesToID(question, session))
+      .toStrictEqual(
+        {schema: 'test',
+        mbql: {
+          source_table:['table1', 1],
+          fields:[],
+          filter: [['=', ['fk->', ['field-id', 123], ['field-id', 321]], 1]],
+          breakout: [],
+          aggregation: [],
+          "order-by": [],
+          columns: [['abc',123]],
+          foreign_columns: [['table2', 'cba', 321]]}}
+      );
+    });
+  });
+
+  describe('breakout (group by) & order by tests', () => {
+    test('named breakout columns can be converted to corresponding mbql field IDs' , async () => {
+      question.mbql.source_table = ['table1'];
+      question.mbql.breakout = ['abc']
+      question.mbql.columns = [['abc']];
+
+      callAPI.mockImplementation(() => returnValue);
+      const session = {id: 12345};
+      expect(await mapSQLValuesToID(question, session))
+      .toStrictEqual(
+        {schema: 'test',
+        mbql: {
+          source_table:['table1', 1],
+          fields:[],
+          filter: [],
+          breakout: [['field-id', 123]],
+          aggregation: [],
+          "order-by": [],
+          columns: [['abc',123]],
+          foreign_columns: []}}
+      );
+    });
+
+    test('named breakout columns with foreign keys can be converted to corresponding mbql field IDs' , async () => {
+      question.mbql.source_table = ['table1'];
+      question.mbql.breakout = [['fk->', 'abc', 'table2.cba']]
+      question.mbql.columns = [['abc']];
+      question.mbql.foreign_columns = [['table2', 'cba']];
+
+      callAPI.mockImplementation(() => returnValue);
+      const session = {id: 12345};
+      expect(await mapSQLValuesToID(question, session))
+      .toStrictEqual(
+        {schema: 'test',
+        mbql: {
+          source_table:['table1', 1],
+          fields:[],
+          filter: [],
+          breakout: [['fk->', ['field-id', 123], ['field-id', 321]]],
+          aggregation: [],
+          "order-by": [],
+          columns: [['abc',123]],
+          foreign_columns: [['table2', 'cba', 321]]}}
+      );
+    });
+
+    test('named order-by columns can be converted to corresponding mbql field IDs' , async () => {
+      question.mbql.source_table = ['table1'];
+      question.mbql['order-by'] = ['abc']
+      question.mbql.columns = [['abc']];
+
+      callAPI.mockImplementation(() => returnValue);
+      const session = {id: 12345};
+      expect(await mapSQLValuesToID(question, session))
+      .toStrictEqual(
+        {schema: 'test',
+        mbql: {
+          source_table:['table1', 1],
+          fields:[],
+          filter: [],
+          breakout: [],
+          aggregation: [],
+          "order-by": [['field-id', 123]],
+          columns: [['abc',123]],
+          foreign_columns: []}}
+      );
+    });
+
+    test('named ordery-by columns with foreign keys can be converted to corresponding mbql field IDs' , async () => {
+      question.mbql.source_table = ['table1'];
+      question.mbql['order-by'] = [['ASC', ['fk->', 'abc', 'table2.cba']]]
+      question.mbql.columns = [['abc']];
+      question.mbql.foreign_columns = [['table2', 'cba']];
+
+      callAPI.mockImplementation(() => returnValue);
+      const session = {id: 12345};
+      expect(await mapSQLValuesToID(question, session))
+      .toStrictEqual(
+        {schema: 'test',
+        mbql: {
+          source_table:['table1', 1],
+          fields:[],
+          filter: [],
+          breakout: [],
+          aggregation: [],
+          "order-by": [['ASC', ['fk->', ['field-id', 123], ['field-id', 321]]]],
+          columns: [['abc',123]],
+          foreign_columns: [['table2', 'cba', 321]]}}
+      );
+    });
+  });
+
+  describe('aggregation tests', () => {
+    test('named aggregation columns in simple queries (no fk relations) can be converted to corresponding mbql field IDs' , async () => {
+      question.mbql.source_table = ['table1'];
+      question.mbql.aggregation = [['SUM', 'abc']];
+      question.mbql.columns = [['abc']];
+
+      callAPI.mockImplementation(() => returnValue);
+      const session = {id: 12345};
+      expect(await mapSQLValuesToID(question, session))
+      .toStrictEqual(
+        {schema: 'test',
+        mbql: {
+          source_table:['table1', 1],
+          fields:[],
+          filter: [],
+          breakout: [],
+          aggregation: [['SUM', ['field-id', 123]]],
+          "order-by": [],
+          columns: [['abc',123]],
+          foreign_columns: []}}
+      );
+    });
+
+    test('named aggregation columns with fk relations can be converted to corresponding mbql field IDs' , async () => {
+      question.mbql.source_table = ['table1'];
+      question.mbql.aggregation = [['SUM', ['fk->', 'abc', 'table2.cba']]];
+      question.mbql.columns = [['abc']];
+      question.mbql.foreign_columns = [['table2', 'cba']];
+
+      callAPI.mockImplementation(() => returnValue);
+      const session = {id: 12345};
+      expect(await mapSQLValuesToID(question, session))
+      .toStrictEqual(
+        {schema: 'test',
+        mbql: {
+          source_table:['table1', 1],
+          fields:[],
+          filter: [],
+          breakout: [],
+          aggregation: [['SUM', ['fk->', ['field-id', 123], ['field-id', 321]]]],
+          "order-by": [],
+          columns: [['abc', 123]],
+          foreign_columns: [['table2', 'cba', 321]]}}
+      );
+    });
   });
 });
