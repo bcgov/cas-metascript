0e224063834551aa6a6f49a2dbf21625d8b0eff2
Dylan Leard <dylan@button.is>
2019-08-12 16:34:52 -0700

    fixed get broken questions test

--- test/unit/get_broken_questions.test.js
+++ test/unit/get_broken_questions.test.js
@@ -1,14 +1,14 @@
 const getBrokenQuestions = require('../get_broken_questions');
 const callAPI = require('../../api_calls/call_api');
+const getSession = require('../../api_calls/get_session');
 const mockedEnv = require('mocked-env');
 
 jest.mock('../../api_calls/call_api');
-restore = mockedEnv({
-  SESSION: process.env.TEST_SESSION
-});
+jest.mock('../../api_calls/get_session');
 
 describe('getQuestionsFromMetabase Integration', () => {
   test('getBrokenQuestions exits with an error if there are broken questions ' , async () => {
+    getSession.mockImplementation(() => {id: 12345})
     callAPI.mockImplementationOnce(() => [{id: 1, name:'broken'}]);
     callAPI.mockImplementation(() => qdata = {error: 'error'});
     const exit = jest.spyOn(process, 'exit').mockImplementation(number => number);
@@ -17,6 +17,7 @@ describe('getQuestionsFromMetabase Integration', () => {
   });
 
   test('getBrokenQuestions exits with an error if there are broken questions ' , async () => {
+    getSession.mockImplementation(() => {id: 12345})
     callAPI.mockImplementationOnce(() => [{id: 1, name:'not_broken'}]);
     callAPI.mockImplementation(() => qdata = {data: 'data'});
     const exit = jest.spyOn(process, 'exit').mockImplementation(number => number);
@@ -24,4 +25,3 @@ describe('getQuestionsFromMetabase Integration', () => {
     expect(exit).toHaveBeenCalledWith(0);
   });
 });
-restore();
