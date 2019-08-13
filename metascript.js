6bc2592f25eac0c632754b14a431628f3e82a79d
Dylan Leard <dylan@button.is>
2019-08-13 15:43:56 -0700

    added errors for incorrect option use in wrapper

--- metascript.js
+++ metascript.js
@@ -24,6 +24,23 @@ program
 .action((pull) => {
   if (pull.debug) { console.log(pull.opts()); }
 
+  // Option Errors
+  // *********************
+  if (!pull.databaseId || (!pull.questionDestination && !pull.dashboardDestination)) {
+    console.error('Required: databaseId (-i <id>) and one of questionDestination (-q <destination) or dashboardDestination (-d <destination>)')
+    console.log(pull.opts());
+    console.log(pull.help());
+    process.exit(1);
+  }
+
+  if (pull.questionDestination && pull.dashboardDestination) {
+    console.error('Option conflict: questionDestination (-q <destination>) and dashboardDestination (-d <destination>) are mutually exclusive')
+    console.log(pull.opts());
+    console.log(pull.help());
+    process.exit(1);
+  }
+  // **********************
+
   if (pull.questionDestination) {
     (async () => { 
       try {
@@ -56,14 +73,38 @@ program
 .description('push entities to Metabase')
 .option('-D, --debug', 'show options')
 .option('-i, --database-id <id>', 'Metabase database id')
-.option('-q, --questionDirectory <dir>', 'push questions to metabase from directory')
-.option('-d, --dashboardDirectory <dir>', 'push dashboards to metabase from directory')
+.option('-q, --question-directory <dir>', 'push questions to metabase from directory')
+.option('-d, --dashboard-directory <dir>', 'push dashboards to metabase from directory')
 .option('-l, --entity-list <list>', 'push a set of questions / dashboards (default: [] pushes all)', [])
 .option('-s, --save', 'save new instances of the entities to metabase (new metabase IDs)')
 .option('-e, --edit', 'save edited instances of the entities in place (keep current metabase IDs)')
 .option('-B, --ignore-broken-question-check', 'ignores the broken question check and proceeds anyway')
 .action((push) => {
   if (push.debug) { console.log(push.opts()); }
+
+  // Option Errors
+  // *********************
+  if (!push.databaseId || (!push.questionDirectory && !push.dashboardDirectory) || (!push.save && !push.edit)) {
+    console.error('Required: databaseId (-i <id>), one of questionDestination (-q <destination) or dashboardDestination (-d <destination>) and one of save (-s) or edit (-e)')
+    console.log(push.opts());
+    console.log(push.help());
+    process.exit(1);
+  }
+
+  if (push.questionDirectory && push.dashboardDirectory) {
+    console.error('Option conflict: questionDestination (-q <destination>) and dashboardDestination (-d <destination>) are mutually exclusive');
+    console.log(push.opts());
+    console.log(push.help());
+    process.exit(1);
+  }
+
+  if (push.save && push.edit) {
+    console.error('Option conflict: save (-s) and edit (-e) are mutually exclusive');
+    console.log(push.opts());
+    console.log(push.help());
+    process.exit(1);
+  }
+  // **********************
 
   if (push.questionDirectory) {
     (async () => {
