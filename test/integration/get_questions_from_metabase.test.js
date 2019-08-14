6b502d0a59c3c20abd7620c9d3a521bfa7250e07
Dylan Leard <dylan@button.is>
2019-08-14 09:56:51 -0700

    updated get questions integration test

--- test/integration/get_questions_from_metabase.test.js
+++ test/integration/get_questions_from_metabase.test.js
@@ -2,44 +2,41 @@ const getQuestionsFromMetabase = require('../../get_questions_from_metabase');
 const mockedEnv = require('mocked-env');
 const rmdir = require('rimraf');
 const fs = require('fs');
+const path = require('path');
 const util = require('util');
 
 test('placeholder', () => {
   expect(1+1).toBe(2);
 });
 
-// restore = mockedEnv({
-//   QUESTION_PATH: './test_questions',
-//   SESSION: process.env.TEST_SESSION
-// });
+const directory = path.join(__dirname, 'test_metabase_directory');
 
-// describe('getQuestionsFromMetabase Integration', () => {
-//   test('getQuestionsFromMetabase creates a folder to house the questions' , async () => {
-//     await getQuestionsFromMetabase([1]);
-//     expect(fs.existsSync(process.env.QUESTION_PATH)).toBe(true);
-//   });
+afterAll(() => rmdir(directory, error => {}));
 
-//   test('getQuestionsFromMetabase creates folders that mirror the collections on metabase' , async () => {
-//     expect(fs.existsSync(`${process.env.QUESTION_PATH}/root`)).toBe(true);
-//     expect(fs.existsSync(`${process.env.QUESTION_PATH}/root/1`)).toBe(true);
-//     expect(fs.existsSync(`${process.env.QUESTION_PATH}/root/2`)).toBe(true);
-//     expect(fs.existsSync(`${process.env.QUESTION_PATH}/root/3`)).toBe(true);
-//   });
+describe('getQuestionsFromMetabase Integration', () => {
+  test('getQuestionsFromMetabase creates a folder to house the questions' , async () => {
+    await getQuestionsFromMetabase({questionDestination: directory, entityList: [1],}, []);
+    expect(fs.existsSync(directory)).toBe(true);
+  });
 
-//   test('getQuestionsFromMetabase saves a specified question when given a question id as a parameter' , async () => {
-//     const question = JSON.parse(fs.readFileSync(`${process.env.QUESTION_PATH}/root/3/1.json`));
-//     expect(question.id).toBe(1);
-//   });
+  test('getQuestionsFromMetabase creates folders that mirror the collections on metabase' , async () => {
+    expect(fs.existsSync(`${directory}/root`)).toBe(true);
+    expect(fs.existsSync(`${directory}/root/1`)).toBe(true);
+    expect(fs.existsSync(`${directory}/root/2`)).toBe(true);
+    expect(fs.existsSync(`${directory}/root/3`)).toBe(true);
+  });
 
-//   test('getQuestionsFromMetabase creates a folder to house the questions' , async () => {
-//     await getQuestionsFromMetabase([]);
-//     expect(fs.existsSync(`${process.env.QUESTION_PATH}/root/3/1.json`)).toBe(true);
-//     expect(fs.existsSync(`${process.env.QUESTION_PATH}/root/3/4.json`)).toBe(true);
-//     expect(fs.existsSync(`${process.env.QUESTION_PATH}/root/2/3.json`)).toBe(true);
-//     expect(fs.existsSync(`${process.env.QUESTION_PATH}/root/2/5.json`)).toBe(true);
-//     expect(fs.existsSync(`${process.env.QUESTION_PATH}/root/2/6.json`)).toBe(true);
-//   });
-// });
+  test('getQuestionsFromMetabase saves a specified question when given a question id as a parameter' , async () => {
+    const question = JSON.parse(fs.readFileSync(`${directory}/root/3/1.json`));
+    expect(question.id).toBe(1);
+  });
 
-// rmdir(process.env.QUESTION_PATH, error => {});
-// restore();
+  test('getQuestionsFromMetabase creates a folder to house the questions' , async () => {
+    await getQuestionsFromMetabase({questionDestination: directory, entityList: [],}, [5]);
+    expect(fs.existsSync(`${directory}/root/3/1.json`)).toBe(true);
+    expect(fs.existsSync(`${directory}/root/3/4.json`)).toBe(true);
+    expect(fs.existsSync(`${directory}/root/2/3.json`)).toBe(true);
+    expect(fs.existsSync(`${directory}/root/2/5.json`)).toBe(false);
+    expect(fs.existsSync(`${directory}/root/2/6.json`)).toBe(true);
+  });
+});
