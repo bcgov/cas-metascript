147df6755990b3a3fa5a884117c73bb154793935
Dylan Leard <dylan@button.is>
2019-08-14 13:14:42 -0700

    testing circle env variables

--- get_questions_from_metabase.js
+++ get_questions_from_metabase.js
@@ -13,7 +13,7 @@ require('dotenv').config();
  */
 async function getQuestionsFromMetabase(args, brokenIDs){
   console.log(process.env.CIRCLE_TEST_ENV);
-  console.log(typeof process.env.CIRCLE_TEST_SESSION);
+  console.log(process.env.CIRCLE_TEST_SESSION);
   const session = (process.env.CIRCLE_TEST_ENV) ? JSON.parse(process.env.CIRCLE_TEST_SESSION) : await getSession();
   console.log(session.id);
   const database_id = args.databaseId;
