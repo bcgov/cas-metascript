8839eeb31260056596759e86b9a21c9ec42d1157
Dylan Leard <dylan@button.is>
2019-08-15 15:41:47 -0700

    update test comments

--- test/integration/save_dashboards_to_metabase.test.js
+++ test/integration/save_dashboards_to_metabase.test.js
@@ -11,9 +11,8 @@ jest.setTimeout(30000);
 const directory = path.join(__dirname, 'save_dashboards_directory');
 const session = (process.env.CIRCLE_TEST_ENV) ? JSON.parse(process.env.CIRCLE_METABASE_SESSION) : JSON.parse(process.env.TEST_SESSION);
 
-// TODO: rewrite descriptions
-describe('saveDashboardsToMetabase Integration', () => {
-  test('on save=true, saveDashboardsToMetabase saves specified dashboard(s) to metabase with a new id if entityList is not empty' , async () => {
+describe('Test saveDashboardsToMetabase Integration', () => {
+  test('when entityList is not empty && save=true, saveDashboardsToMetabase saves specified dashboard(s) to metabase with a new id' , async () => {
     await saveDashboardsToMetabase({dashboardDirectory: directory, save:true, entityList:[1]});
 
     let allDashboards = await callAPI(session, '/dashboard/', 'GET', null, {database: 1});
@@ -28,15 +27,15 @@ describe('saveDashboardsToMetabase Integration', () => {
     await callAPI(session, `/dashboard/${newDashboardID}`, 'DELETE', null, {database: 1});
   });
 
-  xtest('on edit=true, saveDashboardsToMetabase edits specified dashboards(s) in place and saves to metabase if entityList is not empty' , async () => {
+  xtest('when entityList is not empty && edit=true, saveDashboardsToMetabase edits specified dashboards(s) in place and saves to metabase' , async () => {
 
   });
 
-  xtest('on save=true, saveDashboardsToMetabase saves all dashboards in directory to metabase with a new id if entityList is empty', async () => {
+  xtest('when entityList is empty && save=true, saveDashboardsToMetabase saves all dashboards in directory to metabase with a new id', async () => {
 
   });
 
-  xtest('on edit=true, saveDashboardsToMetabase saves all dashboards in directory to metabase with a new id if entityList is empty', async () => {
+  xtest('when entityList is empty && edit=true, saveDashboardsToMetabase saves all dashboards in directory to metabase with a new id', async () => {
     
   });
 });
